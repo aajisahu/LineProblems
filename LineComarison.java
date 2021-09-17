4ebf935878abcd14d1ec32064b9a397e335a2c90
Ashish Sahu <aajisahu@gmail.com>
2021-09-17 23:57:18 +0530

    adding line comparison problem

---
+++ LineComarison.java
@@ -0,0 +1,13 @@
+package LineProblems;
+
+public class LineComarison {
+
+	public static void main(String[] args) {
+		int x1,x2,y1,y2;
+	    double dis;
+	    x1=1;y1=1;x2=4;y2=4;
+	    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+            System.out.println("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+	}
+	
+}
